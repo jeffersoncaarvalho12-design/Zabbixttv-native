ca22a70f5e47fe00bcf32fca46bd55e39a7bf973
jeffersoncaarvalho12-design <jeffersoncaarvalho12@gmail.com>
2026-04-03 18:03:12 -0300

    Update MapRepository.kt

--- app/src/main/java/com/technet/olttv/MapRepository.kt
+++ app/src/main/java/com/technet/olttv/MapRepository.kt
@@ -1,11 +1,8 @@
 package com.technet.olttv
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import java.util.concurrent.TimeUnit
 
 interface MapApiService {
     @GET("olttv/api/mapa-tv.php")
@@ -14,20 +11,8 @@ interface MapApiService {
 
 class MapRepository {
 
-    private val logging = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-    }
-
-    private val client = OkHttpClient.Builder()
-        .connectTimeout(15, TimeUnit.SECONDS)
-        .readTimeout(15, TimeUnit.SECONDS)
-        .writeTimeout(15, TimeUnit.SECONDS)
-        .addInterceptor(logging)
-        .build()
-
     private val api = Retrofit.Builder()
         .baseUrl("http://200.106.207.64:5009/")
-        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(MapApiService::class.java)
